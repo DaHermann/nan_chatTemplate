43c51fbd9b439753628db40056ec3d147bccfd28
tanothierry <tano.tano@uvci.edu.ci>
2020-07-15 16:52:35 +0000

    new

--- app-assets/js/scripts/pages/script.js
+++ app-assets/js/scripts/pages/script.js
@@ -14,3 +14,22 @@ notifaction.addEventListener("click", function () {
     this.classList.add(toggleOn);
   }
 });
+
+let adminli = document.querySelectorAll(".adminli");
+
+let btnli = document.querySelectorAll(".button");
+
+adminli.forEach((li, index) => {
+  li.style.transition = "all ease-in-out .5s";
+  li.style.borderRadius = "3px";
+  li.addEventListener("mouseenter", function () {
+    this.style.background = "whitesmoke";
+    btnli[index].innerHTML = `<i class="fa fa-ellipsis-h"></i>`;
+  });
+  li.addEventListener("mouseleave", function () {
+    this.style.background = "inherit";
+    index !== 0
+      ? (btnli[index].innerHTML = `Admin`)
+      : (btnli[index].innerHTML = ``);
+  });
+});
